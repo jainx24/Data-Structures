624bda54fb6483501d4a09f72e04366c5b53250f
jainx24 <jain.dipesh34@gmail.com>
2017-12-09 18:24:32 +0530

    Add files via upload

---
+++ Trees/LowestCommonAncestor.java
@@ -0,0 +1,32 @@
+/* 
+Binary Search Tree : Lowest Common Ancestor
+
+Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+
+static Node lca(Node root,int v1, int v2)
+{
+        if(root==null)
+            return null;
+            
+        if(root.data==v1 || root.data==v2)
+            return root;
+    
+        Node left = lca(root.left,v1,v2);
+        Node right = lca(root.right,v1,v2);
+    
+        if(left!=null && right !=null)
+            return root;
+    
+        return (left==null)?right:left;    
+}
+
+
+
+
